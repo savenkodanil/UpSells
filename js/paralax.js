b1e5093968cbfcded9dd819786f9f51dfeb3a31e
undead <savenkoblackniga@gmail.com>
2022-08-15 22:05:56 +0300

    paralax

--- js/paralax.js
+++ js/paralax.js
@@ -1,9 +1,11 @@
-$('body').on('mousemove', (e) => {
-    const x = e.pageX / $(window).width();
-    const y = e.pageY / $(window).height();
-
-    $('.paralax').css (
-        'transform',
-        'translate(-' + x * 30 + 'px, -' + y * 30 + 'px)'
-    );
-});
+let w = window.screen.availWidth;
+if(w > 1000){
+    let img1 = document.querySelector('.paralax1')
+    let img2 = document.querySelector('.paralax2')
+    window.addEventListener('mousemove', function(e) {
+        let x = e.clientX / window.innerWidth;
+        let y = e.clientY / window.innerHeight;
+        img1.style.transform = 'translate(-' + x * 20 + 'px, -' + y * 20 + 'px)'
+        img2.style.transform = 'translate(-' + x * 20 + 'px, -' + y * 20 + 'px)'
+    });
+};
